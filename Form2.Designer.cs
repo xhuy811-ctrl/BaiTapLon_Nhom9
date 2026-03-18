656a69006d0a6d4d4d4572b150806aa4028b5046
PhungQuangKy <phungquangky260306@gmail.com>
2026-03-18 21:01:02 +0700

    Kydz

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQuanLy));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tpTaiKhoan = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -87,7 +88,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tpTaiKhoan.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -119,6 +120,7 @@
             // 
             // tpTaiKhoan
             // 
+            this.tpTaiKhoan.Controls.Add(this.button7);
             this.tpTaiKhoan.Controls.Add(this.button6);
             this.tpTaiKhoan.Controls.Add(this.button5);
             this.tpTaiKhoan.Controls.Add(this.textBox2);
@@ -137,6 +139,15 @@
             this.tpTaiKhoan.TabIndex = 0;
             this.tpTaiKhoan.Text = "Tài khoản";
             this.tpTaiKhoan.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(555, 76);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
@@ -674,14 +685,14 @@
             this.button3.Text = "Thoát";
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // button6
+            // button7
             // 
-            this.button6.Location = new System.Drawing.Point(555, 76);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 20;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button7.Location = new System.Drawing.Point(684, 76);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
             // 
             // frmQuanLy
             // 
@@ -774,5 +785,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
